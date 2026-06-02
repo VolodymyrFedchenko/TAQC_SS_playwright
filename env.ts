49587477a0682541b691f90285f90e77d3e888be
Volodymyr Fedchenko <emirwtf@gmail.com>
2026-06-02 22:10:48 +0300

    first test

--- env.ts
+++ env.ts
@@ -5,7 +5,9 @@ dotenv.config({path: path.resolve(__dirname, '..', '.env')});
 
 const BASE_UI_URL = process.env.BASE_UI_URL || 'https://playwright.dev/';
 const HEADLESS = process.env.HEADLESS === 'true'
+const USER_EMAIL = process.env.USER_EMAIL
+const USER_PASSWORD = process.env.USER_PASSWORD
 
 
 
-export {BASE_UI_URL, HEADLESS}
+export {BASE_UI_URL, HEADLESS, USER_EMAIL, USER_PASSWORD}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,20 +1,40 @@
 import { test, expect } from '@playwright/test';
+import { USER_EMAIL, USER_PASSWORD } from "../env"
 
-test('has title', async ({ page }) => {
-  // await page.goto('https://playwright.dev/');
-  await page.goto('/');
+test('Create News form — basic field presence and order', async ({ page }) => {
+  
+  
+  // 1. Відкрити сторінку
+  await page.goto('https://www.greencity.cx.ua/#/greenCity/news');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  // 2. Логін 
+  await page.locator('img.ubs-header-sing-in-img').click();
+  await page.locator('input[formcontrolname="email"]').fill('emir@ua.fm');
+  await page.locator('input[formcontrolname="password"]').fill('SuperVova#1');
+  await page.locator('button[type="submit"]').click();
 
-test('get started link', async ({ page }) => {
-  // await page.goto('https://playwright.dev/');
-  await page.goto('/');
+  // 3. Натиснути Create News  
+  await page.locator('header a.url-name:has-text("Eco news")').click();
+  await page.locator('div#create-button').click();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // 4. Локатори форми
+  const title = page.locator('textarea[formcontrolname="title"]');
+  const tags = page.locator('.tag-button');
+  const addImage = page.locator('input[type="file"]');
+  const mainText = page.locator('div.ql-editor');  
+  const author = page.locator('p:has(span:has-text("author"))');
+  const date = page.locator('p:has(span:has-text("author"))');
+  const source = page.locator('input[formcontrolname="source"]');
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // 5. Перевірка наявності
+  await expect(title).toBeVisible();
+  await expect(tags.first()).toBeVisible();
+  await expect(addImage).toBeVisible();
+  await expect(mainText).toBeVisible();
+  await expect(author).toBeVisible();
+  await expect(date).toBeVisible();
+  await expect(source).toBeVisible();
+
+  // 6. Перевірка порядку елементів
+ 
 });
