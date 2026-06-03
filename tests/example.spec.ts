cc60deacd321567ec76b2a379e78d3021f20451e
Volodymyr Fedchenko <emirwtf@gmail.com>
2026-06-03 23:24:42 +0300

    add new test and allure

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,9 +17,13 @@ test('Create News form — basic field presence and order', async ({ page }) => 
   await page.locator('header a.url-name:has-text("Eco news")').click();
   await page.locator('div#create-button').click();
 
+   //  3.1Очікуємо відкриття форми
+   const form = page.locator('form');
+   await expect(form).toBeVisible();
+
   // 4. Локатори форми
   const title = page.locator('textarea[formcontrolname="title"]');
-  const tags = page.locator('.tag-button');
+  const tags = page.locator('.tags-box').first();
   const addImage = page.locator('input[type="file"]');
   const mainText = page.locator('div.ql-editor');  
   const author = page.locator('p:has(span:has-text("author"))');
@@ -28,13 +32,38 @@ test('Create News form — basic field presence and order', async ({ page }) => 
 
   // 5. Перевірка наявності
   await expect(title).toBeVisible();
-  await expect(tags.first()).toBeVisible();
+  await expect(tags).toBeVisible();
   await expect(addImage).toBeVisible();
   await expect(mainText).toBeVisible();
   await expect(author).toBeVisible();
   await expect(date).toBeVisible();
   await expect(source).toBeVisible();
 
-  // 6. Перевірка порядку елементів
+   // 5.1 Перевірка лічильника Title
+   await expect(page.getByText('0/170')).toBeVisible();
+
+  // 6. Перевірка тегів
+
+  const allTags = ['News', 'Events', 'Education', 'Initiatives', 'Ads'];
+
+  for (const tag of allTags) {
+    await expect(
+      page.getByRole('button', { name: tag })
+    ).toBeVisible();
+  };
+
+   // 7. Перевірка кнопок
+   
+   await expect(
+    page.getByRole('button', { name: 'Cancel' }).nth(1)
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('button', { name: 'Preview' })
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('button', { name: 'Publish' })
+  ).toBeVisible();   
  
 });
